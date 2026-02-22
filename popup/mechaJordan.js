f52909232bfb962d2c2f622842ec38d85c0ab347
chisschwartz <christopherschwartz559@gmail.com>
2026-02-22 15:44:54 -0600

    added button handling, need to get page logic locked down

--- popup/mechaJordan.js
+++ popup/mechaJordan.js
@@ -1,5 +1,32 @@
 function listenForClicks() {
-    document.addEventListener("click", (e) ={
+    document.addEventListener("click", (e) => {
+
+        function intiate(tabs) {
+            browser.tabs.sendMessage(tabs[0].id, {
+                command: "intitate",
+            })
+        }
+
+        function stop(tabs) {
+            browser.tabs.sendMessage(tabs[0].id, {
+                command: "stop",
+            });
+        };
+
         
-    })
+        if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
+            return;
+        }
+
+        if  (e.target.type === "stop") {
+            browser.tabs
+            .query({ active: true, currentWindow: true})
+            .then(stop)
+            
+        } else {
+            browser.tabs
+            .query({ active: true, currentWindow: true})
+            .then(intiate)
+        }
+    });
 }
