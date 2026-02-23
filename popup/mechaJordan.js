4a27609739ab61447ec07e51a7f69e676b9fb3b7
Chris Schwartz <christopherschwartz559@gmail.com>
2026-02-23 11:16:37 -0600

    fixed icon, buttons will not load

--- popup/mechaJordan.js
+++ popup/mechaJordan.js
@@ -13,20 +13,38 @@ function listenForClicks() {
             });
         };
 
-        
+        function getError(error) {
+            console.error(`Could not intiate: ${error}`);
+        }
+
+
         if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
             return;
         }
 
-        if  (e.target.type === "stop") {
+        if (e.target.type === "stop") {
             browser.tabs
-            .query({ active: true, currentWindow: true})
-            .then(stop)
-            
+                .query({ active: true, currentWindow: true })
+                .then(stop)
+                .catch(getError);
+
+
         } else {
             browser.tabs
-            .query({ active: true, currentWindow: true})
-            .then(intiate)
+                .query({ active: true, currentWindow: true })
+                .then(intiate)
+                .catch(getError);
         }
     });
 }
+
+function reportScriptError(error) {
+    document.querySelector("#popup-content").classList.add("hidden");
+    document.querySelector("#error-content").classList.remove("hidden");
+    console.error(`Could not intiate: ${error}`);
+}
+
+browser.tabs
+.executeScript({ file: "/scripts/intiateJordan.js"})
+.then(listenForClicks)
+.catch(reportScriptError);
--- scripts/intiateJordan.js
+++ scripts/intiateJordan.js
@@ -6,23 +6,30 @@
 
     //sets a variable that lets us turn on and off the script
     let isRunning = false;
+    // let randomNum = 0;
 
     //generates random number between 1-10 for grabbing purposes
     function getRandomNum(isRunning) {
         while (isRunning == true) {
-            return Math.floor(Math.random() * 10);
+            number =  Math.floor(Math.random() * 10);
+            return number;
         }
     }
+
+    // while (isRunning == true) {
+    //     randomNum = Math.floor(Math.random() * 10);
+    // }
 
     //commands for the buttons to start and stop the script
     browser.runtime.onMessage.addListener((message) => {
         if(message.command === "intiate"){
             isRunning = true;
             getRandomNum(isRunning);
-            console.log(getRandomNum(isRunning))
+            console.log(getRandomNum(isRunning));
 
         } else if (message.command === "stop") {
             isRunning = false;
+            getRandomNum(isRunning);
         }
     })
 })
